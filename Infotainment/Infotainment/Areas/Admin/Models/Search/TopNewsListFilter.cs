29c767b8e1ad258d73a2ab59b0e137d8a876abac
work-ranjeet <work.ranjeet@gmail.com>
2015-10-31 16:18:55 +0530

    added

---
+++ Infotainment/Infotainment/Areas/Admin/Models/Search/TopNewsListFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Infotainment.Data.Controls;
+
+namespace Infotainment.Areas.Admin.Models
+{
+    public class TopNewsListFilter
+    {
+        [Required]
+        public DateTime DateFrom { get; set; }
+
+        [Required]
+        public DateTime DateTo { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool IsApproved { get; set; }
+
+        [Required]
+        public string Header { get; set; }
+
+        public ConcurrentBag<ITopNews> TopNewsList { get; set; }
+    }
+}
