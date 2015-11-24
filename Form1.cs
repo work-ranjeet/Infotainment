5a5ea649bb3f4333b5d44fc2b215bf2932019116
Ranjeet Kumar <ranjeet.kumar@thomsonreuters.com>
2015-11-24 17:53:38 +0530

    Added

---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace RSAEncryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        #region-----Encryptionand Decryption Function-----
+        static public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+
+        }
+
+        static public byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+
+                return null;
+            }
+
+        }
+        #endregion
+
+        #region--variables area
+        UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+        byte[] plaintext;
+        byte[] encryptedtext;
+        #endregion
+
+        #region-- Function Implemantation
+        private void button1_Click(object sender, EventArgs e)
+        {
+            plaintext = ByteConverter.GetBytes(txtplain.Text);
+            encryptedtext = Encryption(plaintext, RSA.ExportParameters(false), false);
+            txtencrypt.Text = ByteConverter.GetString(encryptedtext);
+           
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] decryptedtex = Decryption(encryptedtext, RSA.ExportParameters(true), false);
+            txtdecrypt.Text = ByteConverter.GetString(decryptedtex);
+        }
+        #endregion
+    }
+}
