e63fff2d51f36e249115f613f0e7f6158226e152
work-ranjeet <work.ranjeet@gmail.com>
2015-11-30 03:41:45 +0530

    dded

---
+++ Infotainment/Infotainment/ExtendedControls/ExtendedControls.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+
+namespace Baidyanath.Extension
+{
+    public static partial class ExtensionMethod
+    {
+        public static HtmlString Literal(this HtmlHelper htmHelper, string Text)
+        {
+            return new HtmlString(Text);
+        }
+    }
+}
