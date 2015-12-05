368782db5708e78800c13b1ad218caf5c71e3c1c
work-ranjeet <work.ranjeet@gmail.com>
2015-12-05 22:34:49 +0530

    added

---
+++ UnitTest/RssServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Infotainment.Data.Common.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class RssServiceTest
+    {
+        [TestMethod]
+        public void RssService()
+        {
+            var result = RssProviderService.Instance.GetTopNewsHeader();
+        }
+    }
+}
